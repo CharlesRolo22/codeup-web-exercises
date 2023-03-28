168570e1281931a4691a608fcb362ceab4137f15
charlesrolo22 <charles.a.roland@gmail.com>
2023-03-28 10:42:57 -0500

    message

--- js/jquery.js
+++ js/jquery.js
@@ -26,3 +26,52 @@
 // $('#header').click(function() {
 //     alert('h1 with id "header" was clicked');
 // });
+
+// $(document).keyup(function(event){
+//     console.log(event.keyCode);
+// });
+
+// console.log('h1').html();
+
+let h1 =$('h1');
+
+// h1.html("attributes and css lecture");
+h1.click(function(){
+    alert($(this).html());
+})
+
+
+// $('#header').click(function() {
+//     let h1Contents = $('#header').html();
+//     alert(h1Contents);
+// })
+
+// $('#container').click(function(){
+//    $(this).html('Codeup Rocks!');
+// });
+
+
+
+// ------ css mode ------
+//------- method chaining -------
+// $('.codeup').css('background', 'red').css('color', 'white');
+// $(".codeup").css({
+//     'background': 'green',
+//     'color': 'white'
+// })
+
+
+// let importantStyling ={
+//     'background': 'green',
+//     'color': 'white'
+// };
+// $(".codeup").css(importantStyling);
+
+
+// $('p').addClass('content')
+
+let para = $('p');
+
+para.click(function (){
+    $(this).toggle('content')
+})
